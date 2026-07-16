5513e624e7ca514ce1f509ae63846055200cca91
anushkahonale-spec <anushkahonale@gmail.com>
2026-07-16 14:59:07 +0530

    Create MajorityElement.java

---
+++ Arrays/MajorityElement.java
@@ -0,0 +1,27 @@
+public class MajorityElement {
+
+    public static void main(String[] args) {
+
+        int[] arr = {2, 2, 1, 1, 1, 2, 2};
+
+        int count = 0;
+        int candidate = 0;
+
+        // Find candidate
+        for (int i = 0; i < arr.length; i++) {
+
+            if (count == 0) {
+                candidate = arr[i];
+            }
+
+            if (arr[i] == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        // Print majority element
+        System.out.println("Majority element is: " + candidate);
+    }
+}
